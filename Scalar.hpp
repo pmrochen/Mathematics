a199589cdbe9b50b34aabb0010536f027f19d92e
Paweł Mrocheń <pmrochen@users.noreply.github.com>
2026-02-27 16:39:36 +0100

    Add lerp, abs, and integer log2 functions

--- Scalar.hpp
+++ Scalar.hpp
@@ -5,100 +5,138 @@
 
 #pragma once
 
+#include <limits>
+#include <type_traits>
 #include <concepts>
 #include <utility>
+#include <bit>
 #include <cmath>
 #include "Constants.hpp"
-
-#ifndef MATHEMATICS_API
-#if defined(MATHEMATICS_DLL) || defined(CORE_DLL)
-#if defined(MATHEMATICS_EXPORTS) || defined(CORE_EXPORTS)
-#define MATHEMATICS_API __declspec(dllexport)
-#else
-#define MATHEMATICS_API __declspec(dllimport)
-#endif
-#else
-#define MATHEMATICS_API
-#endif
-#endif
-
-#ifndef MATHEMATICS_FAST_NORMALIZE
-#define MATHEMATICS_FAST_NORMALIZE 1
-#endif
 
 namespace core::mathematics {
 
 template<typename T>
 	requires (std::floating_point<T> || std::integral<T>)
-inline T sign(T x)
+inline T abs(T x) noexcept
+{
+	if constexpr (std::is_float_v<T>)
+	{
+		return std::fabs(x);
+	}
+	else if constexpr (std::is_signed_v<T>)
+	{
+#if defined(__GNUC__) || defined(__clang__)
+		if constexpr (std::is_same_v<T, int>)
+			return __builtin_abs(x);
+		else if constexpr (std::is_same_v<T, long>)
+			return __builtin_labs(x);
+		else if constexpr (std::is_same_v<T, long long>)
+			return __builtin_llabs(x);
+#endif
+		T y = x >> std::numeric_limits<T>::digits;
+		return (x + y) ^ y;
+	}
+	else // unsigned integer
+	{
+		return x;
+	}
+}
+
+template<typename T>
+	requires (std::floating_point<T> || std::integral<T>)
+inline T sign(T x) noexcept
 { 
 	return (x > T(0)) ? T(1) : ((x < T(0)) ? T(-1) : T(0));
 }
 
 template<std::floating_point T>
-inline T frac(T x)
+inline T frac(T x) noexcept
 { 
 	return x - std::floor(x);
 }
 
 template<std::floating_point T>
-inline bool approxEquals(T a, T b)
+inline bool approxEquals(T a, T b) noexcept
 { 
 	return (std::fabs(b - a) < Constants<T>::TOLERANCE);
 }
 
 template<std::floating_point T>
-inline bool approxEquals(T a, T b, T tolerance)
+inline bool approxEquals(T a, T b, T tolerance) noexcept
 { 
 	return (std::fabs(b - a) < tolerance); 
 }
 
 //template<std::floating_point T>
-//inline bool isNan(T x) 
+//inline bool isNan(T x) noexcept
 //{ 
 //	return !(x == x); 
 //}
 
 template<std::floating_point T>
-inline T log(T x, T b) // logarithm for a base b
+inline T log(T x, T b) noexcept // logarithm for a base b
 { 
 	return std::log(x)/std::log(b);
 }
 
-template<std::floating_point T>
-inline T log2(T x) // base-2 logarithm
+template<typename T>
+	requires (std::floating_point<T> || std::integral<T>)
+inline T log2(T x) noexcept // base-2 logarithm
 { 
-	return std::log(x)/Constants<T>::LN2; 
+	if constexpr (std::is_float_v<T>)
+	{
+		return std::log(x)/Constants<T>::LN2;
+	}
+	else
+	{
+		if constexpr (std::is_signed_v<T>)
+			x -= x & (x >> std::numeric_limits<T>::digits); // branchless max(x, 0)
+#if defined(__GNUC__) || defined(__clang__)
+		if constexpr (std::is_same_v<T, unsigned int> || std::is_same_v<T, int>)
+			return T(sizeof(T)*8 - 1 - __builtin_clz((unsigned int)x));
+		else if constexpr (std::is_same_v<T, unsigned long> || std::is_same_v<T, long>)
+			return T(sizeof(T)*8 - 1 - __builtin_clzl((unsigned long)x));
+		else if constexpr (std::is_same_v<T, unsigned long long> || std::is_same_v<T, long long>)
+			return T(sizeof(T)*8 - 1 - __builtin_clzll((unsigned long long)x));
+#endif
+		return T(sizeof(T)*8 - 1 - std::countl_zero(std::make_unsigned_t<T>(x)));
+	}
 }
 
 template<std::floating_point T>
-inline T radians(T x)
+inline T radians(T x) noexcept
 { 
 	return x*Constants<T>::DEG_TO_RAD; 
 }
 
 template<std::floating_point T>
-inline T degrees(T x)
+inline T degrees(T x) noexcept
 { 
 	return x*Constants<T>::RAD_TO_DEG; 
 }
 
+template<std::floating_point T>
+inline T lerp(T a, T b, T t) noexcept
+{ 
+	return a + t*(b - a);
+}
+
 template<typename T>
 	requires (std::floating_point<T> || std::integral<T>)
-inline T step(T a, T t)
+inline T step(T a, T t) noexcept
 { 
 	return (t >= a) ? T(1) : T(0); 
 }
 
 template<typename T>
 	requires (std::floating_point<T> || std::integral<T>)
-inline T pulse(T a, T b, T t)
+inline T pulse(T a, T b, T t) noexcept
 { 
 	return step(a, t) - step(b, t); 
 }
 
 template<std::floating_point T>
-inline T boxStep(T a, T b, T t)
+inline T boxStep(T a, T b, T t) noexcept
 {
 	if (t <= a)
 		return T(0);
@@ -108,7 +146,7 @@ inline T boxStep(T a, T b, T t)
 }
 
 template<std::floating_point T>
-inline T smoothStep(T a, T b, T t)
+inline T smoothStep(T a, T b, T t) noexcept
 {
 	if (t <= a)
 		return T(0);
@@ -119,7 +157,7 @@ inline T smoothStep(T a, T b, T t)
 }
 
 template<std::floating_point T>
-inline T smootherStep(T a, T b, T t)
+inline T smootherStep(T a, T b, T t) noexcept
 {
 	if (t <= a)
 		return T(0);
