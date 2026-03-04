ce47ea84d8dd9edc00bada2de8fde456404f5dcb
Paweł Mrocheń <pmrochen@users.noreply.github.com>
2026-03-04 16:46:42 +0100

    Add AxisAlignedBox and Sphere classes

---
+++ AxisAlignedBox.hpp
@@ -0,0 +1,473 @@
+/*
+ *	Name: AxisAlignedBox
+ *	Author: Pawel Mrochen
+ */
+
+#pragma once
+
+#include <istream>
+#include <ostream>
+#include <type_traits>
+#include <concepts>
+#include <utility>
+#include <tuple>
+#include <algorithm>
+#include <cstddef>
+#include <cmath>
+#include "Constants.hpp"
+#include "Vector3.hpp"
+#include "Matrix3.hpp"
+#include "AffineTransform.hpp"
+#include "HalfSpace.hpp"
+#include "Plane.hpp"
+#include "Triangle3.hpp"
+
+namespace core::mathematics {
+namespace templates {
+
+template<typename T>
+	requires std::floating_point<T>
+struct OrientedBox;
+
+template<typename T>
+	requires std::floating_point<T>
+struct Sphere;
+
+template<typename T>
+	requires std::floating_point<T>
+struct SymmetricFrustum;
+
+template<typename T>
+	requires std::floating_point<T>
+struct AxisAlignedBox
+{
+	using Real = T;
+	using ConstArg = const AxisAlignedBox&;
+	using ConstResult = const AxisAlignedBox&;
+
+	AxisAlignedBox() noexcept : minimum(Vector3<T>::INF), maximum(Vector3<T>::MINUS_INF) {}
+	explicit AxisAlignedBox(Uninitialized) noexcept : minimum(Uninitialized()), maximum(Uninitialized()) {}
+	AxisAlignedBox(const Vector3<T>& minimum, const Vector3<T>& maximum) noexcept : minimum(minimum), maximum(maximum) {}
+	explicit AxisAlignedBox(const Vector3<T>& dimensions) noexcept : minimum(T(-0.5)* dimensions), maximum(T(0.5)* dimensions) {}
+	explicit AxisAlignedBox(const std::pair<Vector3<T>, Vector3<T>>& t) noexcept : minimum(t.first), maximum(t.second) {}
+	explicit AxisAlignedBox(const std::tuple<Vector3<T>, Vector3<T>>& t) noexcept : minimum(std::get<0>(t)), maximum(std::get<1>(t)) {}
+	explicit AxisAlignedBox(const OrientedBox<T>& box) noexcept;
+
+	//explicit operator std::pair<Vector3<T>, Vector3<T>>() { return { minimum, maximum }; }
+	//explicit operator std::tuple<Vector3<T>, Vector3<T>>() { return { minimum, maximum }; }
+	bool operator==(const AxisAlignedBox& box) const noexcept { return (minimum == box.minimum) && (maximum == box.maximum); }
+	bool operator!=(const AxisAlignedBox& box) const noexcept { return !(*this == box); }
+
+	template<typename A> void serialize(A& ar) { ar(minimum, maximum); }
+
+	// Properties
+	bool isEmpty() const noexcept { return minimum.anyGreaterThan(maximum); }
+	bool approxEquals(const AxisAlignedBox& box) const noexcept;
+	bool approxEquals(const AxisAlignedBox& box, T tolerance) const noexcept;
+	bool isFinite() const noexcept { return minimum.isFinite() && maximum.isFinite(); }
+	AxisAlignedBox& makeEmpty() noexcept { minimum = Vector3<T>::INF; maximum = Vector3<T>::MINUS_INF; return *this; }
+	AxisAlignedBox& set(const Vector3<T>& minimum, const Vector3<T>& maximum) noexcept { this->minimum = minimum; this->maximum = maximum; return *this; }
+	const Vector3<T>& getMinimum() const noexcept { return minimum; }
+	void setMinimum(const Vector3<T>& minimum) noexcept { this->minimum = minimum; }
+	const Vector3<T>& getMaximum() const noexcept { return maximum; }
+	void setMaximum(const Vector3<T>& maximum) noexcept { this->maximum = maximum; }
+	Vector3<T> getDimensions() const noexcept { return (maximum - minimum); }
+	void setDimensions(const Vector3<T>& dimensions) noexcept;
+	Vector3<T> getHalfDimensions() const noexcept { return (maximum - minimum)*T(0.5); }
+	void setHalfDimensions(const Vector3<T>& halfDims) noexcept;
+	Vector3<T> getCenter() const noexcept { return (minimum + maximum)*T(0.5); }
+	void setCenter(const Vector3<T>& center) noexcept;
+	T getDiagonal() const noexcept { return distance(minimum, maximum); }
+	T getArea() const noexcept;
+	T getVolume() const noexcept;
+
+	// Vertices
+	template<std::output_iterator<Vector3<T>> O> O copyVertices(O target) const noexcept;
+	std::vector<Vector3> getVertices() const;
+
+	// Primitives
+	template<std::integral U> std::pair<const U*, const U*> getPrimitives(int nVerticesPerPrimitive) const noexcept; // #TODO return range
+	std::size_t getPrimitiveCount(int nVerticesPerPrimitive) const noexcept;
+
+	// Half spaces
+	template<std::output_iterator<HalfSpace<T>> O> O copyHalfSpaces(O target) const noexcept;
+	std::vector<HalfSpace> getHalfSpaces() const;
+
+	// Circumscribed sphere
+	Sphere<T> getCircumscribedSphere() const noexcept;
+
+	// Transformation
+	AxisAlignedBox& inflate(const Vector3<T>& halfDims) noexcept { minimum -= halfDims; maximum += halfDims; return *this; }
+	AxisAlignedBox& translate(const Vector3<T>& offset) noexcept { minimum += offset; maximum += offset; return *this; }
+	AxisAlignedBox& transform(const Matrix3<T>& matrix, bool orthogonal = false) noexcept;
+	AxisAlignedBox& transform(const AffineTransform<T>& transformation, bool orthogonal = false) noexcept;
+
+	// Union and intersection
+	AxisAlignedBox& setUnion(const AxisAlignedBox& a, const AxisAlignedBox& b) noexcept;
+	AxisAlignedBox& setIntersection(const AxisAlignedBox& a, const AxisAlignedBox& b) noexcept;
+	static AxisAlignedBox makeUnion(const AxisAlignedBox& a, const AxisAlignedBox& b) noexcept { return AxisAlignedBox(Uninitialized()).setUnion(a, b); }
+	static AxisAlignedBox makeIntersection(const AxisAlignedBox& a, const AxisAlignedBox& b) noexcept { return AxisAlignedBox(Uninitialized()).setIntersection(a, b); }
+	AxisAlignedBox& extendBy(const Vector3<T>& point) noexcept;
+	AxisAlignedBox& extendBy(const AxisAlignedBox& box) noexcept { return setUnion(*this, box); }
+	AxisAlignedBox& extendBy(const Sphere<T>& sphere) noexcept;
+
+	// Closest point
+	Vector3<T> getClosestPoint(const Vector3<T>& point) const noexcept { return clamp(point, minimum, maximum); }
+	T getDistanceTo(const Vector3<T>& point) const noexcept { return distance(point, getClosestPoint(point)); }
+
+	// Containment and intersection
+	bool contains(const Vector3<T>& point) const noexcept;
+	bool contains(const AxisAlignedBox& box) const noexcept;
+	bool contains(const Sphere<T>& sphere) const noexcept;
+	//int classify(const HalfSpace<T>& halfSpace) const noexcept; // -1 = inside, 1 = outside, 0 = partial // #TODO return enum
+	bool intersects(const HalfSpace<T>& halfSpace) const noexcept;
+	bool intersects(const Plane<T>& plane) const noexcept;
+	bool intersects(const Triangle3<T>& triangle) const noexcept;
+	bool intersects(const AxisAlignedBox& box) const noexcept;
+	bool intersects(const OrientedBox<T>& box) const noexcept;
+	bool intersects(const Sphere<T>& sphere) const noexcept;
+	bool intersects(const SymmetricFrustum<T>& frustum) const noexcept;
+
+	static const AxisAlignedBox EMPTY;
+
+	Vector3<T> minimum;
+	Vector3<T> maximum;
+};
+
+template<typename T> const AxisAlignedBox<T> AxisAlignedBox<T>::EMPTY{ Vector3<T>::INF, Vector3<T>::MINUS_INF };
+
+template<typename C, typename T, typename U>
+	requires std::floating_point<U>
+inline std::basic_istream<C, T>& operator>>(std::basic_istream<C, T>& s, AxisAlignedBox<U>& box)
+{ 
+	return s >> box.minimum >> std::ws >> box.maximum;
+}
+
+template<typename C, typename T, typename U>
+	requires std::floating_point<U>
+inline std::basic_ostream<C, T>& operator<<(std::basic_ostream<C, T>& s, const AxisAlignedBox<U>& box)
+{ 
+	constexpr C WS(0x20);
+	return s << box.minimum << WS << box.maximum;
+}
+
+template<typename T>
+inline bool AxisAlignedBox<T>::approxEquals(const AxisAlignedBox<T>& box) const
+{
+	return minimum.approxEquals(box.minimum) && maximum.approxEquals(box.maximum);
+}
+
+template<typename T>
+inline bool AxisAlignedBox<T>::approxEquals(const AxisAlignedBox<T>& box, T tolerance) const
+{
+	return minimum.approxEquals(box.minimum, tolerance) && maximum.approxEquals(box.maximum, tolerance);
+}
+
+template<typename T>
+inline void AxisAlignedBox<T>::setDimensions(const Vector3<T>& dimensions)
+{
+	Vector3<T> center = (minimum + maximum)*T(0.5);
+	Vector3<T> halfDims = T(0.5)*dimensions;
+	minimum = center - halfDims;
+	maximum = center + halfDims;
+}
+
+template<typename T>
+inline void AxisAlignedBox<T>::setHalfDimensions(const Vector3<T>& halfDims)
+{
+	Vector3<T> center = (minimum + maximum)*T(0.5);
+	minimum = center - halfDims;
+	maximum = center + halfDims;
+}
+
+template<typename T>
+inline void AxisAlignedBox<T>::setCenter(const Vector3<T>& center)
+{
+	Vector3<T> diff = center - (minimum + maximum)*T(0.5);
+	minimum += diff;
+	maximum += diff;
+}
+
+template<typename T>
+inline T AxisAlignedBox<T>::getArea() const
+{
+	Vector3<T> dim = maximum - minimum;
+	return T(2)*(dim.x*dim.y + dim.y*dim.z + dim.z*dim.x); // T(2)*dot(dim, dim.yzx())
+}
+
+template<typename T>
+inline T AxisAlignedBox<T>::getVolume() const
+{
+	Vector3<T> dim = maximum - minimum;
+	return dim.x*dim.y*dim.z;
+}
+
+template<typename T>
+template<std::output_iterator<Vector3<T>> O> 
+inline O AxisAlignedBox<T>::copyVertices(O target) const
+{
+	*target++ = minimum;
+	(target++)->set(maximum.x, minimum.y, minimum.z);
+	(target++)->set(minimum.x, maximum.y, minimum.z);
+	(target++)->set(maximum.x, maximum.y, minimum.z);
+	(target++)->set(minimum.x, minimum.y, maximum.z);
+	(target++)->set(maximum.x, minimum.y, maximum.z);
+	(target++)->set(minimum.x, maximum.y, maximum.z);
+	*target++ = maximum;
+	return target;
+}
+
+template<typename T>
+inline std::vector<Vector3<T>> AxisAlignedBox<T>::getVertices() const
+{
+	std::vector<Vector3<T>> vertices;
+	vertices.resize(8);
+	vertices[0] = minimum;
+	vertices[1].set(maximum.x, minimum.y, minimum.z);
+	vertices[2].set(minimum.x, maximum.y, minimum.z);
+	vertices[3].set(maximum.x, maximum.y, minimum.z);
+	vertices[4].set(minimum.x, minimum.y, maximum.z);
+	vertices[5].set(maximum.x, minimum.y, maximum.z);
+	vertices[6].set(minimum.x, maximum.y, maximum.z);
+	vertices[7] = maximum;
+	return vertices;
+}
+
+template<typename T>
+template<std::integral U> 
+std::pair<const U*, const U*> AxisAlignedBox<T>::getPrimitives(int nVerticesPerPrimitive) const
+{
+	static const U edges[24] = { 0, 2, 2, 3, 3, 1, 1, 0, 3, 7, 5, 1, 6, 2, 0, 4, 5, 7, 7, 6, 6, 4, 4, 5 };
+	static const U triangles[36] = { 0, 2, 1, 3, 1, 2, 1, 3, 5, 7, 5, 3, 5, 7, 4, 6, 4, 7, 4, 6, 0, 2, 0, 6, 2, 6, 3, 7, 3, 6, 4, 0, 5, 1, 5, 0 };
+	static const U quads[24] = { 0, 2, 3, 1, 1, 3, 7, 5, 5, 7, 6, 4, 4, 6, 2, 0, 2, 6, 7, 3, 4, 0, 1, 5 };
+
+	switch (nVerticesPerPrimitive)
+	{
+		case 2:
+			return { edges, edges + 24 };
+		case 3:
+			return { triangles, triangles + 36 };
+		case 4:
+			return { quads, quads + 24 };
+		default:
+			return { nullptr, nullptr };
+	}
+}
+
+template<typename T>
+inline std::size_t AxisAlignedBox<T>::getPrimitiveCount(int nVerticesPerPrimitive) const
+{
+	switch (nVerticesPerPrimitive)
+	{
+		case 2:
+			return 12;
+		case 3:
+			return 12;
+		case 4:
+			return 6;
+		default:
+			return 0;
+	}
+}
+
+template<typename T>
+template<std::output_iterator<HalfSpace<T>> O>
+inline O AxisAlignedBox<T>::copyHalfSpaces(O target) const
+{
+	*target++ = HalfSpace<T>(Vector3<T>::MINUS_UNIT_X, Vector3<T>(minimum.x, T(0), T(0)));
+	*target++ = HalfSpace<T>(Vector3<T>::UNIT_X, Vector3<T>(maximum.x, T(0), T(0)));
+	*target++ = HalfSpace<T>(Vector3<T>::MINUS_UNIT_Y, Vector3<T>(T(0), minimum.y, T(0)));
+	*target++ = HalfSpace<T>(Vector3<T>::UNIT_Y, Vector3<T>(T(0), maximum.y, T(0)));
+	*target++ = HalfSpace<T>(Vector3<T>::MINUS_UNIT_Z, Vector3<T>(T(0), T(0), minimum.z));
+	*target++ = HalfSpace<T>(Vector3<T>::UNIT_Z, Vector3<T>(T(0), T(0), maximum.z));
+	return target;
+}
+
+template<typename T>
+inline std::vector<HalfSpace<T>> AxisAlignedBox<T>::getHalfSpaces() const
+{
+	std::vector<HalfSpace<T>> halfSpaces;
+	halfSpaces.resize(6);
+	halfSpaces[0] = HalfSpace<T>(Vector3<T>::MINUS_UNIT_X, Vector3<T>(minimum.x, T(0), T(0)));
+	halfSpaces[1] = HalfSpace<T>(Vector3<T>::UNIT_X, Vector3<T>(maximum.x, T(0), T(0)));
+	halfSpaces[2] = HalfSpace<T>(Vector3<T>::MINUS_UNIT_Y, Vector3<T>(T(0), minimum.y, T(0)));
+	halfSpaces[3] = HalfSpace<T>(Vector3<T>::UNIT_Y, Vector3<T>(T(0), maximum.y, T(0)));
+	halfSpaces[4] = HalfSpace<T>(Vector3<T>::MINUS_UNIT_Z, Vector3<T>(T(0), T(0), minimum.z));
+	halfSpaces[5] = HalfSpace<T>(Vector3<T>::UNIT_Z, Vector3<T>(T(0), T(0), maximum.z));
+	return halfSpaces;
+}
+
+template<typename T>
+inline AxisAlignedBox<T>& AxisAlignedBox<T>::setUnion(const AxisAlignedBox<T>& a, const AxisAlignedBox<T>& b)
+{
+	minimum.setMinimum(a.minimum, b.minimum);
+	maximum.setMaximum(a.maximum, b.maximum);
+	return *this;
+}
+
+template<typename T>
+inline AxisAlignedBox<T>& AxisAlignedBox<T>::setIntersection(const AxisAlignedBox<T>& a, const AxisAlignedBox<T>& b)
+{
+	minimum.setMaximum(a.minimum, b.minimum);
+	maximum.setMinimum(a.maximum, b.maximum);
+	return *this;
+}
+
+template<typename T>
+inline AxisAlignedBox<T>& AxisAlignedBox<T>::extendBy(const Vector3<T>& point)
+{
+	minimum.setMinimum(minimum, point);
+	maximum.setMaximum(maximum, point);
+	return *this;
+}
+
+template<typename T>
+inline bool AxisAlignedBox<T>::contains(const Vector3<T>& point) const
+{
+	return minimum.allLessThanEqual(point) && maximum.allGreaterThanEqual(point);
+}
+
+template<typename T>
+inline bool AxisAlignedBox<T>::contains(const AxisAlignedBox<T>& box) const
+{
+	return minimum.allLessThanEqual(box.minimum) && maximum.allGreaterThanEqual(box.maximum);
+}
+
+template<typename T>
+inline bool AxisAlignedBox<T>::intersects(const HalfSpace<T>& halfSpace) const
+{
+	// #TODO LUT-based implementation
+	Vector3<T> halfDims = (maximum - minimum)*T(0.5);
+	Vector3<T> center = (minimum + maximum)*T(0.5);
+	Vector3<T> normal = halfSpace.getNormal();
+	T r = sum(abs(halfDims*normal));
+	return ((dot(normal, center) + halfSpace.d) <= r);
+}
+
+template<typename T>
+inline bool AxisAlignedBox<T>::intersects(const Plane<T>& plane) const
+{
+	// #TODO LUT-based implementation
+	Vector3<T> halfDims = (maximum - minimum)*T(0.5);
+	Vector3<T> center = (minimum + maximum)*T(0.5);
+	Vector3<T> normal = plane.getNormal();
+	T r = sum(abs(halfDims*normal));
+	return (std::fabs(dot(normal, center) + plane.d) <= r);
+}
+
+template<typename T>
+inline bool AxisAlignedBox<T>::intersects(const Triangle3<T>& triangle) const
+{
+	Vector3 center = (minimum + maximum)*T(0.5);
+	return intersections::testAxisAlignedBoxTriangle((maximum - minimum)*T(0.5), // #TODO
+		triangle.vertices[0] - center, triangle.vertices[1] - center, triangle.vertices[2] - center);
+}
+
+template<typename T>
+inline bool AxisAlignedBox<T>::intersects(const AxisAlignedBox<T>& box) const
+{
+	return minimum.allLessThanEqual(box.maximum) && maximum.allGreaterThanEqual(box.minimum);
+}
+
+} // namespace templates
+
+#if MATHEMATICS_DOUBLE
+using AxisAlignedBox = templates::AxisAlignedBox<double>;
+using AxisAlignedBoxArg = templates::AxisAlignedBox<double>::ConstArg;
+using AxisAlignedBoxResult = templates::AxisAlignedBox<double>::ConstResult;
+#else
+using AxisAlignedBox = templates::AxisAlignedBox<float>;
+using AxisAlignedBoxArg = templates::AxisAlignedBox<float>::ConstArg;
+using AxisAlignedBoxResult = templates::AxisAlignedBox<float>::ConstResult;
+#maximumif
+
+} // namespace core::mathematics
+
+namespace std {
+
+template<typename T>
+struct hash;
+
+template<typename T>
+struct hash<::core::mathematics::templates::AxisAlignedBox<T>>
+{
+	std::size_t operator()(const ::core::mathematics::templates::AxisAlignedBox<T>& box) const noexcept
+	{
+		std::hash<T> hasher;
+		std::size_t seed = hasher(box.minimum) + 0x9e3779b9;
+		seed ^= hasher(box.maximum) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+		return seed;
+	}
+};
+
+} // namespace std
+
+#include "OrientedBox.hpp"
+#include "Sphere.hpp"
+#include "SymmetricFrustum.hpp"
+
+namespace core::mathematics::templates {
+
+template<typename T>
+inline AxisAlignedBox<T>::AxisAlignedBox(const OrientedBox<T>& box)
+{
+	Vector3<T> halfDims = abs(box.halfDims.x*box.basis[0]) + abs(box.halfDims.y*box.basis[1]) + abs(box.halfDims.z*box.basis[2]);
+	minimum = box.center - halfDims;
+	maximum = box.center + halfDims;
+}
+
+template<typename T>
+inline Sphere<T> AxisAlignedBox<T>::getCircumscribedSphere() const
+{
+	Vector3<T> center = (minimum + maximum)*T(0.5);
+	return Sphere<T>(center, distance(center, maximum));
+}
+
+template<typename T>
+inline AxisAlignedBox<T>& AxisAlignedBox::transform(const Matrix3<T>& matrix, bool orthogonal)
+{
+	*this = AxisAlignedBox<T>(OrientedBox<T>(*this, matrix, orthogonal));
+	return *this;
+}
+
+template<typename T>
+inline AxisAlignedBox<T>& AxisAlignedBox::transform(const AffineTransform<T>& transformation, bool orthogonal)
+{
+	*this = AxisAlignedBox<T>(OrientedBox<T>(*this, transformation, orthogonal));
+	return *this;
+}
+
+template<typename T>
+inline AxisAlignedBox<T>& AxisAlignedBox<T>::extendBy(const Sphere<T>& sphere)
+{
+	Vector3<T> radius(sphere.radius);
+	setUnion(*this, AxisAlignedBox<T>(sphere.center - radius, sphere.center + radius));
+	return *this;
+}
+
+template<typename T>
+inline bool AxisAlignedBox<T>::contains(const Sphere<T>& sphere) const
+{
+	Vector3<T> radius(sphere.radius);
+	return minimum.allLessThanEqual(sphere.center - radius) && maximum.allGreaterThanEqual(sphere.center + radius);
+}
+
+template<typename T>
+inline bool AxisAlignedBox<T>::intersects(const OrientedBox<T>& box) const
+{
+	return box.intersects(*this);
+}
+
+template<typename T>
+inline bool AxisAlignedBox<T>::intersects(const Sphere<T>& sphere) const
+{
+	return sphere.intersects(*this);
+}
+
+template<typename T>
+inline bool AxisAlignedBox<T>::intersects(const SymmetricFrustum<T>& frustum) const
+{
+	return frustum.intersects(*this);
+}
+
+} // namespace core::mathematics::templates
---
+++ Sphere.hpp
@@ -0,0 +1,245 @@
+/*
+ *	Name: Sphere
+ *	Author: Pawel Mrochen
+ */
+
+#pragma once
+
+#include <istream>
+#include <ostream>
+#include <type_traits>
+#include <concepts>
+#include <utility>
+#include <algorithm>
+#include <cstddef>
+#include <cmath>
+#include "Constants.hpp"
+#include "Scalar.hpp"
+#include "Vector3.hpp"
+#include "HalfSpace.hpp"
+#include "Plane.hpp"
+#include "Triangle3.hpp"
+#include "AxisAlignedBox.hpp"
+#include "OrientedBox.hpp"
+
+namespace core::mathematics {
+namespace templates {
+
+template<typename T>
+	requires std::floating_point<T>
+struct Ellipsoid;
+
+template<typename T>
+	requires std::floating_point<T>
+struct Cone;
+
+template<typename T>
+	requires std::floating_point<T>
+struct SymmetricFrustum;
+
+template<typename T>
+	requires std::floating_point<T>
+struct Sphere
+{
+	using Real = T;
+	using ConstArg = const Sphere&;
+	using ConstResult = const Sphere&;
+
+	Sphere() noexcept : center(), radius() {}
+	explicit Sphere(Uninitialized) noexcept : center(Uninitialized()) {}
+	Sphere(const Vector3<T>& center, T radius) noexcept : center(center), radius(radius) {}
+	explicit Sphere(const Ellipsoid<T>& ellipsoid) noexcept;
+
+	bool operator==(const Sphere& sphere) const noexcept { return (center == sphere.center) && (radius == sphere.radius); }
+	bool operator!=(const Sphere& sphere) const noexcept { return !(*this == sphere); }
+
+	template<typename A> void serialize(A& ar) { ar(center, radius); }
+
+	// Properties
+	bool approxEquals(const Sphere& sphere) const noexcept;
+	bool approxEquals(const Sphere& sphere, T tolerance) const noexcept;
+	bool isFinite() const noexcept { return center.isFinite() && radius.isFinite(); }
+	Sphere& set(const Vector3<T>& center, T radius) noexcept { this->center = center; this->radius = radius; return *this; }
+	const Vector3<T>& getCenter() const noexcept { return center; }
+	void setCenter(const Vector3<T>& center) noexcept { this->center = center; }
+	T getRadius() const noexcept { return radius; }
+	void setRadius(T radius) noexcept { this->radius = radius; }
+	T getDiameter() const noexcept { return radius*T(2); }
+	void setDiameter(T diameter) noexcept { radius = diameter*T(0.5); }
+	T getArea() const noexcept { return T(4)*Constants<T>::PI*radius*radius; }
+	T getVolume() const noexcept { return T(4)*Constants<T>::PI*radius*radius*radius/T(3); }
+
+	// Circumscribed box
+	AxisAlignedBox<T> getCircumscribedBox() const noexcept;
+
+	// Transformation
+	Sphere& translate(const Vector3<T>& offset) { center += offset; return *this; }
+
+	// Closest point
+	//Vector3<T> getClosestPoint(const Vector3<T>& point) const noexcept; // #TODO
+	T getDistanceTo(const Vector3<T>& point) const { return std::max(distance(point, center) - radius, T(0)); }
+	T getSignedDistanceTo(const Vector3<T>& point) const noexcept { return distance(point, center) - radius; }
+
+	// Containment and intersection
+	bool contains(const Vector3<T>& point) const noexcept { return (distanceSquared(point, center) <= radius*radius); }
+	bool intersects(const HalfSpace<T>& halfSpace) const noexcept;
+	bool intersects(const Plane<T>& plane) const noexcept;
+	bool intersects(const Triangle3<T>& triangle) const noexcept;
+	bool intersects(const AxisAlignedBox<T>& box) const noexcept;
+	bool intersects(const OrientedBox<T>& box) const noexcept;
+	bool intersects(const Sphere& sphere) const noexcept;
+	bool intersects(const Cone<T>& cone) const noexcept;
+	bool intersects(const SymmetricFrustum<T>& frustum) const noexcept;
+
+	Vector3<T> center;
+	T radius;
+};
+
+template<typename C, typename T, typename U>
+	requires std::floating_point<U>
+inline std::basic_istream<C, T>& operator>>(std::basic_istream<C, T>& s, Sphere<U>& sphere)
+{ 
+	return s >> sphere.center >> std::ws >> sphere.radius;
+}
+
+template<typename C, typename T, typename U>
+	requires std::floating_point<U>
+inline std::basic_ostream<C, T>& operator<<(std::basic_ostream<C, T>& s, const Sphere<U>& sphere)
+{ 
+	constexpr C WS(0x20);
+	return s << sphere.center << WS << sphere.radius;
+}
+
+template<typename T>
+inline bool Sphere<T>::approxEquals(const Sphere<T>& sphere) const
+{
+	return center.approxEquals(sphere.center) && 
+		(std::fabs(sphere.radius - radius) < Constants<T>::TOLERANCE);
+}
+
+template<typename T>
+inline bool Sphere<T>::approxEquals(const Sphere<T>& sphere, T tolerance) const
+{
+	return center.approxEquals(sphere.center, tolerance) && 
+		(std::fabs(sphere.radius - radius) < tolerance);
+}
+
+template<typename T>
+inline AxisAlignedBox<T> Sphere<T>::getCircumscribedBox() const
+{
+	Vector3<T> halfDims(radius);
+	return AxisAlignedBox<T>(center - halfDims, center + halfDims);
+}
+
+template<typename T>
+inline bool Sphere<T>::intersects(const HalfSpace<T>& halfSpace) const
+{
+	return ((dot(halfSpace.getNormal(), center) + halfSpace.d) <= radius);
+}
+
+template<typename T>
+inline bool Sphere<T>::intersects(const Plane<T>& plane) const
+{
+	return (std::fabs(dot(plane.getNormal(), center) + plane.d) <= radius);
+}
+
+template<typename T>
+inline bool Sphere<T>::intersects(const Triangle3<T>& triangle) const
+{
+	return (triangle.getDistanceSquaredTo(center) <= radius*radius);
+}
+
+template<typename T>
+inline bool Sphere<T>::intersects(const AxisAlignedBox<T>& box) const
+{
+	T d = T(0);
+	if (center.x < box.minimum.x)
+		d += sqr(center.x - box.minimum.x);
+	else if (center.x > box.maximum.x)
+		d += sqr(center.x - box.maximum.x);
+	if (center.y < box.minimum.y)
+		d += sqr(center.y - box.minimum.y);
+	else if (center.y > box.maximum.y)
+		d += sqr(center.y - box.maximum.y);
+	if (center.z < box.minimum.z)
+		d += sqr(center.z - box.minimum.z);
+	else if (center.z > box.maximum.z)
+		d += sqr(center.z - box.maximum.z);
+	return (d <= radius*radius);
+}
+
+template<typename T>
+inline bool Sphere<T>::intersects(const OrientedBox<T>& box) const
+{
+	//Matrix3<T> boxBasisT(transpose(box.basis));
+	//Sphere<T> sphere((center - box.center)*boxBasisT, radius);
+	Sphere<T> sphere(box.basis*(center - box.center), radius);
+	return sphere.testIntersection(AxisAlignedBox<T>(-box.halfDims, box.halfDims));
+}
+
+template<typename T>
+inline bool Sphere<T>::intersects(const Sphere<T>& sphere) const
+{
+	T d = sphere.radius + radius;
+	return (distanceSquared(sphere.center, center) <= d*d);
+}
+
+} // namespace templates
+
+#if MATHEMATICS_DOUBLE
+using Sphere = templates::Sphere<double>;
+using SphereArg = templates::Sphere<double>::ConstArg;
+using SphereResult = templates::Sphere<double>::ConstResult;
+#else
+using Sphere = templates::Sphere<float>;
+using SphereArg = templates::Sphere<float>::ConstArg;
+using SphereResult = templates::Sphere<float>::ConstResult;
+#endif
+
+} // namespace core::mathematics
+
+namespace std {
+
+template<typename T>
+struct hash;
+
+template<typename T>
+struct hash<::core::mathematics::templates::Sphere<T>>
+{
+	std::size_t operator()(const ::core::mathematics::templates::Sphere<T>& sphere) const noexcept
+	{
+		std::hash<T> hasher;
+		std::size_t seed = hasher(sphere.center) + 0x9e3779b9;
+		seed ^= hasher(sphere.radius) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+		return seed;
+	}
+};
+
+} // namespace std
+
+#include "Ellipsoid.hpp"
+#include "Cone.hpp"
+#include "SymmetricFrustum.hpp"
+
+namespace core::mathematics::templates {
+
+template<typename T>
+inline Sphere<T>::Sphere(const Ellipsoid<T>& ellipsoid) :
+	center(ellipsoid.center),
+	radius(ellipsoid.radii.getMaxComponent())
+{
+}
+
+template<typename T>
+inline bool Sphere<T>::intersects(const Cone<T>& cone) const
+{
+	return cone.intersects(*this);
+}
+
+template<typename T>
+inline bool Sphere<T>::intersects(const SymmetricFrustum<T>& frustum) const
+{
+	return frustum.intersects(*this);
+}
+
+} // namespace core::mathematics::templates
